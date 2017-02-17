2e05e5f69bd998f81532e8d284fa5dbb70377375
15196004316@163.com <lfy@0916>
2017-02-17 18:23:06 +0800

    更改端口号

--- src/view/topic.vue
+++ src/view/topic.vue
@@ -3,11 +3,32 @@
     <c-header></c-header>
     <div class="content">
       <div class="content-padded">
-        <div class="row">
-          <div class="col-20">20%</div>
-          <div class="col-80">80%</div>
+        <div class="row" v-for="item in items">
+          <div class="col-20 t_avatar">{{item.message}}</div>
+          <div class="col-80 t_title">{{item.message}}</div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+  export default {
+      data() {
+          return {
+              items:[
+                {
+                    message: 'FOO'
+                },
+                {
+                    message: 'Bar'
+                }
+              ]
+          }
+      }
+  }
+</script>
+
+<style>
+
+</style>
